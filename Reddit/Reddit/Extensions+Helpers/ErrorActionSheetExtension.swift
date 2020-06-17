795e408449316d17a41f9837330cf1c4b77fa519
Connor Holland <connorholland@Connors-MacBook-Pro.local>
2020-06-17 14:46:33 -0600

    complete

---
+++ Reddit/Reddit/Extensions+Helpers/ErrorActionSheetExtension.swift
@@ -0,0 +1,9 @@
+//
+//  ErrorActionSheetExtension.swift
+//  Reddit
+//
+//  Created by Connor Holland on 6/17/20.
+//  Copyright © 2020 Connor Holland. All rights reserved.
+//
+
+import Foundation
---
+++ Reddit/Reddit/Views/CustomViews/PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  Reddit
+//
+//  Created by Connor Holland on 6/17/20.
+//  Copyright © 2020 Connor Holland. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
