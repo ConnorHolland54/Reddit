c84067ee1f3ffbc95b4c577aa970c6fcd8077c77
Connor Holland <connorholland@Connors-MacBook-Pro.local>
2020-06-17 10:27:55 -0600

    Initial commit containing new project

---
+++ Reddit/Reddit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reddit
+//
+//  Created by Connor Holland on 6/17/20.
+//  Copyright © 2020 Connor Holland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
